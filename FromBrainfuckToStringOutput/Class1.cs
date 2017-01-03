21a5e455b316ae0660a8a743b5a97d1bcea83de7
matyjb <matyjasiakbartosz@gmail.com>
2017-01-03 18:33:40 +0100

    .

--- FromBrainfuckToStringOutput/Class1.cs
+++ FromBrainfuckToStringOutput/Class1.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FromBrainfuckToStringOutput
+namespace BrainfuckCompiler
 {
-    public class Class1
+    public class Main
     {
+        int pointer = 0;
+        int[] cells;
+        string output = "";
+
+        private static bool CheckCode(string code)
+        {         
+            foreach (char c in code)
+            {
+                if (c != '+' && c != '-' && c != '>' && c != '<' && c != ',' && c != '.' && c != '[' && c != ']')
+                    return false;
+            }
+            //+check loops
+            return true;
+        }
+
+        private static int[] InitializeCells(int from, int to, out int startcell)
+        {
+
+            int size = Math.Abs(from) + Math.Abs(to) + 1;
+            startcell = Math.Abs(from);
+            return new int[size];
+
+        }
+
+        public string Compile(string code)
+        {
+            CheckCode(code); //Repair/message/something
+            cells = InitializeCells(code.Count(f => f == '<'), code.Count(f => f == '>'), out pointer);
+
+
+
+
+
+
+            return output;
+        }
     }
 }
